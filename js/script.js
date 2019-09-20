abef23d766552a038876eb2207244983ffdeb7a8
Bartosz Gromada <bartosz.k.gromada@gmail.com>
2019-09-20 14:09:39 +0200

    new_js_finnaly

--- js/script.js
+++ js/script.js
@@ -8,33 +8,18 @@ const titleClickHandler = function(event){
   const articleSelector = clickedElement.getAttribute('href');
   const targetArticle = document.querySelector(articleSelector);
 
-  console.log(articleSelector);
-  console.log(targetArticle);
-
-  /* [DONE] remove class 'active' from all article links  */
-
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* [DONE] add class 'active' to the clicked link */
-
   clickedElement.classList.add('active');
-
-  /* [DONE] remove class 'active' from all articles */
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-
   targetArticle.classList.add('active');
-  }
+}
 
 const links = document.querySelectorAll('.titles a');
 
